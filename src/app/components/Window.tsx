124eea108d4f9107686c16b782b84ccb096e4de7
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2023-11-11 21:11:08 -0600

    updated window component to be unique to UUID

--- src/app/components/Window.tsx
+++ src/app/components/Window.tsx
@@ -1,39 +1,163 @@
-import React, {useState, useRef, useEffect, useCallback} from 'react';
+import React, {useState, useRef, useEffect, useCallback, cloneElement} from 'react';
 import { Button, Frame, Toolbar, Window, WindowContent, WindowHeader } from 'react95';
 import { set, close } from '../reducers/programSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import { renderToStaticMarkup } from 'react-dom/server';
 import Draggable from 'react-draggable';
 import Resizable from './Resizable';
 
 import styles from '../styles/window.module.scss';
 
+function asyncDelay(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
 const Programs = () => {
 
   const dispatch = useDispatch()
   const tasks = useSelector((state: any) => state.program.tasks)
-  console.log('tasks: ',tasks)
+  const [max, setMax] = useState(false)
+  const [clone, setClone] = useState<HTMLElement>()
+  const [el, setEl] = useState()
+  // console.log('tasks: ',tasks)
 
-  const ref = React.useCallback((node: any) => {
-    node?.focus()
-  }, [])
+  let example: number;
+  let windowRef: Element;
+  const ref = useRef<any>({});
+  const copyRef = useRef<any>({});
+  const maxRef = useRef(false);
+  const topRef = useRef<any>()
+
+  let exampleEl = <div></div>
+  
+  // const ref = React.useCallback((node: any) => { 
+  //   windowRef = node
+  //   node?.focus()
+  //   example = 5;
+  // }, [])
+  // console.log('REF: ',windowRef)
+
+  useEffect(() => {
+    console.log('REF: ',ref)
+    console.log('MAX: ',max)
+    console.log('CLONE: ', clone)
+   
+    if (topRef.current) {
+      if (!clone) {console.log(setClone(topRef.current.cloneNode(true)))}
+      console.log('TOPREF INNERHMTL: ',topRef.current.nodeValue)
+    }
+    
+    if (topRef.current && copyRef.current) {
+      // const rect = topRef.current.getBoundingClientRect();
+      // const x = rect.left + window.scrollX;
+      // const y = rect.top + window.scrollY;
+      // const width = topRef.current.offsetWidth;
+      // const height = topRef.current.offsetHeight;
+
+      // copyRef.current.style.transform = `translate(${x + 2}px, ${y + 2}px)`;
+      // copyRef.current.style.width = `${width - 4}px`;
+      // copyRef.current.style.height = `${height - 4}px`;
+
+      // copyRef.current.style.color = 'red';
+      // console.log('COPYREF: ',copyRef.current)
+    }
+  })
+
+  useEffect(() => {
+    if (copyRef.current && topRef.current) {
+      // matchPosition()
+    }
+  }, [copyRef.current])
 
   return (
     <>
+    {/* {clone ? <div ref={copyRef} id={styles['copy']} dangerouslySetInnerHTML={{ __html: clone!.outerHTML}}/> : ''} */}
+    
       {
         tasks.map((e: any, i: number) => {
 
+          const setRef = (el: any) => {
+            console.log('REF IN FN: ',el)
+            if (el && copyRef.current) { copyRef.current[e.uuid] = el }
+            console.log('COPYREF: ', copyRef)
+          }
+
+          const Copy = () => {
+            if (clone) {
+              // return <div ref={copyRef} id={styles['copy']} dangerouslySetInnerHTML={{ __html: clone!.outerHTML}}/>
+              return <div ref={setRef} id={styles['copy']} dangerouslySetInnerHTML={{ __html: clone!.outerHTML}}/>
+            }
+            // else {return ''}
+          }
+
+          const matchPosition = () => {
+            console.log('COPYREF STYLES IN MP: ', copyRef.current.style)
+            const rect = topRef.current.getBoundingClientRect();
+            const x = rect.left + window.scrollX;
+            const y = rect.top + window.scrollY;
+            const width = topRef.current.offsetWidth;
+            const height = topRef.current.offsetHeight;
+        
+            copyRef.current[e.uuid].style.transform = `translate(${x + 2}px, ${y + 2}px)`;
+            copyRef.current[e.uuid].style.width = `${width - 4}px`;
+            copyRef.current[e.uuid].style.height = `${height - 4}px`;
+            copyRef.current[e.uuid].style.color = 'blue';
+          }
+
           const closeProgram = () => {
-            // dispatch(close(e.uuid))
+            dispatch(close(e.uuid))
+            delete ref.current[e.uuid], copyRef.current[e.uuid];
+            delete copyRef.current[e.uuid]
+          }
+
+          const Maximize = async () => {
+            maxRef.current = !maxRef.current
+            setMax((max)=>!max)
+            console.log('REFS: ',topRef.current, copyRef.current)
+
+            if (topRef.current && copyRef.current) {
+              console.log('running top and copy ref cond')
+              console.log('COPYREF STYLES: ', copyRef.current.style)
+
+              await asyncDelay(1);
+              matchPosition()
+              await asyncDelay(1);
+              copyRef.current[e.uuid].style.display = 'block';
+
+              await asyncDelay(1);
+              if (maxRef.current) {
+                console.log('running maxref cond')
+                copyRef.current[e.uuid].style.width = `100%`;
+                copyRef.current[e.uuid].style.transform = `translate(0)`;
+                copyRef.current[e.uuid].style.color = 'red';
+              }
+              else {
+                matchPosition()
+              }
+
+            }
+
+            await asyncDelay(1000);
+            ref.current[e.uuid].classList.toggle(styles.max);
+            
           }
 
           if (Object.keys(e).length){
             return (
               <>
-                <Draggable key={e.uuid} onDrag={(e) => { console.log('drag event: ', e) }} handle="[class*=title]">
-                  <div ref={ref} className={styles.container} tabIndex={i}>
+                <Copy />
+                <Draggable key={e.uuid} handle={`[class*=title]`} disabled={max}>
+                  <div
+                  ref={element => {
+                  element?.focus()
+                  if (element && ref.current) { ref.current[e.uuid] = element } 
+                  else { delete ref.current[e.uuid] }}} className={styles.container} 
+                  tabIndex={i}>
                     <Resizable>
-                      <Window className={styles.window} >
-                        <WindowHeader className={styles.title}>
+                      <Window className={styles.window} onDrag={()=>{console.log('DRAGGINGGG')}}>
+                        <WindowHeader ref={topRef} className={styles.title} onDoubleClick={Maximize}>
                           <div className={styles.top}>
                             <figure><img src={`/${e.icon}.ico`} alt="" /></figure>
                             <span>{e.title}</span>
@@ -41,7 +165,7 @@ const Programs = () => {
                           <div className={styles.controlBtns}>
                             <div>
                               <button className={styles.min}><span/></button>
-                              <button className={styles.max}><span/></button>
+                              <button onClick={Maximize} className={styles.max}><span/></button>
                             </div>
                             <button onClick={closeProgram}> <span className={styles.close} /> </button>
                           </div>
--- src/app/components/desktop.tsx
+++ src/app/components/desktop.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import TaskBar from './TaskBar';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import DesktopIcon from './DesktopIcon';
 import { AppBar } from 'react95';
 import Programs from './Window';
@@ -11,8 +11,15 @@ import styles from '../styles/desktop.module.scss';
 export function Desktop() {
   const [open, setOpen] = useState(false);
 
+  const desktopRef = useRef<any>()
+
+useEffect(() => {
+  if (desktopRef.current) {desktopRef.current.style.paddingBottom = `${document.querySelector('header')?.offsetHeight}px`}
+})
+
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={desktopRef}>
       <div className={styles.space}>
         <DesktopIcon name={'neighborhood'}/>
         <Programs />
