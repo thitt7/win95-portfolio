eb4a3110d0b61e6c4663f6c02cf6350458cb7604
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2023-11-26 22:10:05 -0600

    updated nextconfig, added staging branch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverActions: true,
+      },
     
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      
+      typescript: {
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        ignoreBuildErrors: true,
+      },
 }
 
 module.exports = nextConfig
