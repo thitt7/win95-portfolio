8dee33be815f9017592e794ef442f0ec99978eba
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2023-12-03 21:39:53 -0600

    resizable styles

--- src/app/components/desktop.tsx
+++ src/app/components/desktop.tsx
@@ -12,7 +12,7 @@ import styles from '../styles/desktop.module.scss';
 export function Desktop() {
   const [open, setOpen] = useState(false);
   const [message, setMessage] = useState({
-    body: `Welcome to my portfolio! I'm Tristan, a full-stack developer who grow up in the early 2000s with Windows 95 - 2000 operating systems as my first foray into desktop computers.
+    body: `Welcome to my portfolio! I'm Tristan, a full-stack developer who grew up in the early 2000s with Windows 95 - 2000 operating systems as my first foray into desktop computers.
       Have a look around the desktop, start menu, etc. it's just like you remember it. I've spared no expense and tried to make this the most true to life recreation of Windows 95 
       short of actually emulating it.`,
     type: "warn",
